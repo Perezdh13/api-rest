23a4624930287faa70bf740ecee273abc830405a
Raul Villar <raultheviking@gmail.com>
2023-03-22 12:14:39 +0100

    Added model

--- src/main/java/com/example/Api/Rest/model/CVoice.java
+++ src/main/java/com/example/Api/Rest/model/CVoice.java
@@ -9,6 +9,53 @@ public class CVoice {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String email;
+    private int price;
+    private String category;
+    private String urlImg;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getUrlImg() {
+        return urlImg;
+    }
+
+    public void setUrlImg(String urlImg) {
+        this.urlImg = urlImg;
+    }
+
+    public String getUrlVideo() {
+        return urlVideo;
+    }
+
+    public void setUrlVideo(String urlVideo) {
+        this.urlVideo = urlVideo;
+    }
+
+    private String urlVideo;
+
 
     public Long getId() {
         return id;
